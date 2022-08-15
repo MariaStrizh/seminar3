51cc997f208954e5fb0fc8bed3444f56ca256d66
Maria <str_maria@mail.ru>
2022-08-15 14:29:42 +0300

    Задачи семинара

---
+++ task17/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine ("Ввердите координаты по X:");
+int X=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Ввердите координаты по Y:");
+int Y=Convert.ToInt32(Console.ReadLine());
+
+if (X>0 && Y>0)
+{
+    Console.WriteLine("Точка находится в 1 четверти");
+}
+else if (X<0 && Y>0)
+{
+    Console.WriteLine("Точка находится во 2 четверти");
+}
+else if (X<0 && Y<0)
+{
+    Console.WriteLine("Точка находится в 3 четверти");
+}
+else if (X>0 && Y<0)
+{
+    Console.WriteLine("Точка находится в 4 четверти");
+}
+else
+{
+    Console.WriteLine("Не можем определить в какой четверти");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты по Х первой точки");
+double X1=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по Y первой точки");
+double Y1=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по Х второй точки");
+double X2=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по Y второй точки");
+double Y2=Convert.ToDouble(Console.ReadLine());
+
+double res=Math.Sqrt(Math.Pow(X1-X2,2)+Math.Pow(Y1-Y2,2));
+Console.WriteLine($"{res:f2}");
